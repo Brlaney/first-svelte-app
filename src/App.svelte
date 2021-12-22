87fa0d0adc272406c598a96c045d3ea86219ab5e
brlaney42 <brlaney@outlook.com>
2021-12-22 13:34:44 -0600

    Finished the basic tutorial

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,22 @@
 <script lang='ts'>
-	import FeedbackList from './components/FeedbackList.svelte'
-	import FeedbackStats from './components/FeedbackStats.svelte'
-
-	let feedback = [
-		{ id: 1, rating: 10,text: 'Enim eu turpis egestas pretium. Amet consectetur adipiscing elit ut aliquam purus sit amet. Leo vel fringilla est ullamcorper eget nulla facilisi etiam.'},
-		{ id: 2, rating: 8,text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et tortor at risus viverra adipiscing. Feugiat nisl pretium fusce id velit ut tortor pretium. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur.'},
-		{ id: 3, rating: 7,text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id neque aliquam vestibulum morbi blandit cursus risus. Ut pharetra sit amet aliquam id diam maecenas ultricies.'},
-	]
-
-	$: count = feedback.length
-	$: average = feedback.reduce((a, {rating}) => a + rating, 0) / feedback.length
-	$: ave = average.toFixed(2)
-
-	const deleteFeedback = (e) => {
-		const itemId = e.detail
-		feedback = feedback.filter((item) => item.id != itemId)
-	}
+	import MaterialButton from './components/MaterialButton.svelte';
 </script>
 
-<main class='container'>
-	<FeedbackStats 
-		{count} 
-		{ave} 
-	/>
-  <FeedbackList 
-	  feedback={feedback} 
-		on:delete-feedback={deleteFeedback}
-	/>
-</main>
+<MaterialButton 
+  id='one' 
+  text='Button no 1'
+	onClickFunction={() => {
+		console.log('You just clicked button no 1')
+	}}
+/>
+<MaterialButton 
+  id='two' 
+  text='Button no 2'
+	onClickFunction={() => {
+		console.log('You just clicked button no 2')
+	}}
+/>
+
+<style global lang='scss'>
+	@use '@material/button/styles';
+</style>
---
+++ src/components/MaterialButton.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from 'svelte';
+  import {MDCRipple} from '@material/ripple';
+
+  onMount(() => {
+    // Code goes here
+    const buttonRipple = new MDCRipple(document.getElementById(id));
+  })
+
+  export let id;
+  export let text;
+  export let onClickFunction;
+</script>
+
+<div class='mdc-touch-target-wrapper'>
+  <button class='mdc-button mdc-button--touch' {id} on:click={onClickFunction}>
+    <span class='mdc-button__ripple' />
+    <span class='mdc-button__touch' />
+    <span class='mdc-button__label'>{text}</span>
+  </button>
+</div>

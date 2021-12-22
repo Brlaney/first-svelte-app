d7a594c419df02bb315798b7de65d3548db5c133
brlaney42 <brlaney@outlook.com>
2021-12-21 19:55:35 -0600

    Ending of brief introduction

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,44 @@
-<script lang="ts">
-	export let name: string;
+<script lang='ts'>
+	let firstName = 'Brendan';
+	let lastName = 'Laney';
+	let color = 'blue';
+	let showText = false;
+	let users = [
+		{ id: 1, name: 'Bo' },
+		{ id: 2, name: 'Bill' },
+		{ id: 3, name: 'Jill' }
+	];
+
+	$: name = firstName + ' ' + lastName;
+
+	const toggle = () => {
+		color = color === 'blue' ? 'red' : 'blue';
+		showText = !showText;
+		users = [...users, { id: 4, name: 'Jenny' }];
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	
+	<h1 style='color: {color}'>Hello {name}!</h1>
+	
+	{#if showText}
+
+		<p>Visit the <a href='https://svelte.dev/tutorial'>
+			Svelte tutorial</a> to learn how to build Svelte apps.
+		</p>
+	
+		{:else}
+		<p>Else case text</p>
+
+	{/if}
+
+	<button on:click={toggle}>Click</button>
+
+	{#each users as user (user.id)}
+		<h3>{user.id}: {user.name}</h3>
+	{/each}
+	
 </main>
 
 <style>
